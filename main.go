693defeaa8db00b126a6c6439911cfeadc16ffae
Matthew Krager <metoofarmer@yahoo.com>
2019-03-06 23:48:47 -0800

    setting up db code

--- main.go
+++ main.go
@@ -1,29 +1,100 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+    "bytes"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "strconv"
+    "time"
+    "database/sql"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+    "github.com/gin-gonic/gin"
+    "github.com/russross/blackfriday"
+    _ "github.com/lib/pq"
 )
 
+var (
+    repeat int
+    db     *sql.DB
+)
+
+func repeatFunc(c *gin.Context) {
+    var buffer bytes.Buffer
+    for i := 0; i < repeat; i++ {
+        buffer.WriteString("Hello from Go!")
+    }
+    c.String(http.StatusOK, buffer.String())
+}
+
+func dbFunc(c *gin.Context) {
+    if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+        c.String(http.StatusInternalServerError,
+            fmt.Sprintf("Error creating database table: %q", err))
+        return
+    }
+
+    if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+        c.String(http.StatusInternalServerError,
+            fmt.Sprintf("Error incrementing tick: %q", err))
+        return
+    }
+
+    rows, err := db.Query("SELECT tick FROM ticks")
+    if err != nil {
+        c.String(http.StatusInternalServerError,
+            fmt.Sprintf("Error reading ticks: %q", err))
+        return
+    }
+
+    defer rows.Close()
+    for rows.Next() {
+        var tick time.Time
+        if err := rows.Scan(&tick); err != nil {
+          c.String(http.StatusInternalServerError,
+            fmt.Sprintf("Error scanning ticks: %q", err))
+            return
+        }
+        c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+    }
+}
+
 func main() {
-	port := os.Getenv("PORT")
+    port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+    if port == "" {
+        log.Fatal("$PORT must be set")
+    }
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+    var err error
+    tStr := os.Getenv("REPEAT")
+    repeat, err = strconv.Atoi(tStr)
+    if err != nil {
+        log.Print("Error converting $REPEAT to an int: %q - Using default", err)
+        repeat = 5
+    }
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+    db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+    if err != nil {
+        log.Fatalf("Error opening database: %q", err)
+    }
 
-	router.Run(":" + port)
+    router := gin.New()
+    router.Use(gin.Logger())
+    router.LoadHTMLGlob("templates/*.tmpl.html")
+    router.Static("/static", "static")
+
+    router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.tmpl.html", nil)
+    })
+
+    router.GET("/mark", func(c *gin.Context) {
+        c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
+    })
+
+    router.GET("/repeat", repeatFunc)
+    router.GET("/db", dbFunc)
+
+    router.Run(":" + port)
 }
