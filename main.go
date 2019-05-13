1adefaced179438873aafda16dcd75b72a95a394
Matthew Krager <matthewkrager@gmail.com>
2019-05-13 11:20:58 -0700

    websocket methods

--- main.go
+++ main.go
@@ -1,100 +1,159 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-    "strconv"
-    "time"
-    "database/sql"
+	"bytes"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 
-    "github.com/gin-gonic/gin"
-    "github.com/russross/blackfriday"
-    _ "github.com/lib/pq"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	_ "github.com/lib/pq"
+	"github.com/russross/blackfriday"
 )
 
 var (
-    repeat int
-    db     *sql.DB
+	repeat int
+	db     *sql.DB
 )
 
+var connected_clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+// Define our message object
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+// Configure the upgrader
+var upgrader = websocket.Upgrader{}
+
 func repeatFunc(c *gin.Context) {
-    var buffer bytes.Buffer
-    for i := 0; i < repeat; i++ {
-        buffer.WriteString("Hello from Go!")
-    }
-    c.String(http.StatusOK, buffer.String())
+	var buffer bytes.Buffer
+	for i := 0; i < repeat; i++ {
+		buffer.WriteString("Hello from Go!")
+	}
+	c.String(http.StatusOK, buffer.String())
 }
 
 func dbFunc(c *gin.Context) {
-    if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
-        c.String(http.StatusInternalServerError,
-            fmt.Sprintf("Error creating database table: %q", err))
-        return
-    }
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+		c.String(http.StatusInternalServerError,
+			fmt.Sprintf("Error creating database table: %q", err))
+		return
+	}
 
-    if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
-        c.String(http.StatusInternalServerError,
-            fmt.Sprintf("Error incrementing tick: %q", err))
-        return
-    }
+	if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+		c.String(http.StatusInternalServerError,
+			fmt.Sprintf("Error incrementing tick: %q", err))
+		return
+	}
 
-    rows, err := db.Query("SELECT tick FROM ticks")
-    if err != nil {
-        c.String(http.StatusInternalServerError,
-            fmt.Sprintf("Error reading ticks: %q", err))
-        return
-    }
+	rows, err := db.Query("SELECT tick FROM ticks")
+	if err != nil {
+		c.String(http.StatusInternalServerError,
+			fmt.Sprintf("Error reading ticks: %q", err))
+		return
+	}
 
-    defer rows.Close()
-    for rows.Next() {
-        var tick time.Time
-        if err := rows.Scan(&tick); err != nil {
-          c.String(http.StatusInternalServerError,
-            fmt.Sprintf("Error scanning ticks: %q", err))
-            return
-        }
-        c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
-    }
+	defer rows.Close()
+	for rows.Next() {
+		var tick time.Time
+		if err := rows.Scan(&tick); err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error scanning ticks: %q", err))
+			return
+		}
+		c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+	}
+}
+
+// Chat methods
+func handleConnections(c *gin.Context) {
+	// Upgrade initial GET request to a socket
+	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
+	// Register our new client
+	connected_clients[ws] = true
+	for {
+		var msg Message
+		// Read in a new message as JSON and map it to a Message object
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(connected_clients, ws)
+			break
+		}
+		// broadcast new message
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		// Send msg to all connected
+		for client := range connected_clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(connected_clients, client)
+			}
+		}
+	}
 }
 
 func main() {
-    port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 
-    if port == "" {
-        log.Fatal("$PORT must be set")
-    }
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
-    var err error
-    tStr := os.Getenv("REPEAT")
-    repeat, err = strconv.Atoi(tStr)
-    if err != nil {
-        log.Print("Error converting $REPEAT to an int: %q - Using default", err)
-        repeat = 5
-    }
+	var err error
+	tStr := os.Getenv("REPEAT")
+	repeat, err = strconv.Atoi(tStr)
+	if err != nil {
+		fmt.Printf("Error converting $REPEAT to an int: %v - Using default\n", err)
+		repeat = 5
+	}
 
-    db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
-    if err != nil {
-        log.Fatalf("Error opening database: %q", err)
-    }
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}
 
-    router := gin.New()
-    router.Use(gin.Logger())
-    router.LoadHTMLGlob("templates/*.tmpl.html")
-    router.Static("/static", "static")
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
 
-    router.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.tmpl.html", nil)
-    })
+	// to start a websocket
+	router.GET("/ws", handleConnections)
+	// Start listening for incoming chat messages
+	go handleMessages()
 
-    router.GET("/mark", func(c *gin.Context) {
-        c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
-    })
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
 
-    router.GET("/repeat", repeatFunc)
-    router.GET("/db", dbFunc)
+	router.GET("/mark", func(c *gin.Context) {
+		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
+	})
 
-    router.Run(":" + port)
+	router.GET("/repeat", repeatFunc)
+	router.GET("/db", dbFunc)
+
+	router.Run(":" + port)
 }
