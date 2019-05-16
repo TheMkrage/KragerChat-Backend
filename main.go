52317683dd07257ff9c71ccaa8d33b794f5d4061
Matthew Krager <matthewkrager@gmail.com>
2019-05-16 13:35:03 -0700

    basic gcloud intergration

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
-	"github.com/russross/blackfriday"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
 )
 
 var (
@@ -31,67 +28,51 @@ type Message struct {
 	Sender         string `json:"sender"`
 	Photo          Photo  `json:"photo"`
 	ConnectionInfo ConnectionInfo
+	ThreadID       int
+	Posted         time.Time
 }
 
-func repeatFunc(c *gin.Context) {
-	var buffer bytes.Buffer
-	for i := 0; i < repeat; i++ {
-		buffer.WriteString("Hello from Go!")
-	}
-	c.String(http.StatusOK, buffer.String())
+type Thread struct {
+	ID   int
+	Name string
 }
 
-func dbFunc(c *gin.Context) {
-	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
-		c.String(http.StatusInternalServerError,
-			fmt.Sprintf("Error creating database table: %q", err))
-		return
-	}
+type Subscription struct {
+	APN_Token string
+	ThreadID  int
+}
 
-	if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
-		c.String(http.StatusInternalServerError,
-			fmt.Sprintf("Error incrementing tick: %q", err))
-		return
-	}
+// POST /threads
+func createThread(c *gin.Context) {
+	ctx := appengine.NewContext(c.Request)
 
-	rows, err := db.Query("SELECT tick FROM ticks")
+	key := datastore.NewIncompleteKey(ctx, "Thread", nil)
+	id, err := datastore.NewQuery("Thread").Count(ctx)
 	if err != nil {
-		c.String(http.StatusInternalServerError,
-			fmt.Sprintf("Error reading ticks: %q", err))
+		c.Writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	defer rows.Close()
-	for rows.Next() {
-		var tick time.Time
-		if err := rows.Scan(&tick); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error scanning ticks: %q", err))
-			return
-		}
-		c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+	thread := Thread{ID: id, Name: "Chatroom 1"}
+	if _, err := datastore.Put(ctx, key, thread); err != nil {
+		c.Writer.WriteHeader(http.StatusInternalServerError)
+		return
 	}
+	c.JSON(http.StatusCreated, thread)
+}
+
+// POST /threads/:id/join
+func joinThread(c *gin.Context) {
+	//ctx := appengine.NewContext(c.Request)
+	//id := c.Param("id")
 }
 
 func main() {
-	//os.Setenv("PORT", "8000")
+	os.Setenv("PORT", "8000")
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
-	}
-
-	var err error
-	tStr := os.Getenv("REPEAT")
-	repeat, err = strconv.Atoi(tStr)
-	if err != nil {
-		fmt.Printf("Error converting $REPEAT to an int: %v - Using default\n", err)
-		repeat = 5
-	}
-
-	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatalf("Error opening database: %q", err)
 	}
 
 	// checks origin before allowing upgrade to connection
@@ -99,24 +80,13 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
 
-	// to start a websocket
+	router.POST("/threads/:id/join", joinThread)
+	router.POST("/threads", createThread)
 	router.GET("/ws", handleConnections)
+
 	// Start listening for incoming chat messages
 	go handleMessages()
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.GET("/mark", func(c *gin.Context) {
-		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
-	})
-
-	router.GET("/repeat", repeatFunc)
-	router.GET("/db", dbFunc)
 
 	router.Run(":" + port)
 }
